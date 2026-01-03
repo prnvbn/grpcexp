779345a99d985c889c2588497321aba82cc4573e
prnvbn <prnvbn@gmail.com>
2026-01-03 21:05:27 +0000

    add support for method listing in tui

---
+++ internal/tui/methods_list.go
@@ -0,0 +1,61 @@
+package tui
+
+import (
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+var _ list.Item = &methodItem{}
+
+type MethodsList struct {
+	list        list.Model
+	serviceName string
+}
+
+func NewMethodsList(serviceName string, methods []protoreflect.MethodDescriptor) MethodsList {
+	items := make([]list.Item, len(methods))
+	for i, method := range methods {
+		items[i] = methodItem{method}
+	}
+
+	l := list.New(items, minimalDelegate{}, 0, 0)
+	l.Title = serviceName
+	l.SetShowTitle(false)
+	l.SetShowStatusBar(false)
+	l.SetShowFilter(true)
+	l.SetShowHelp(true)
+	l.SetShowPagination(false)
+
+	return MethodsList{
+		list:        l,
+		serviceName: serviceName,
+	}
+}
+
+func (m *MethodsList) SetSize(width, height int) {
+	m.list.SetSize(width, height)
+}
+
+func (m *MethodsList) Update(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return cmd
+}
+
+func (m *MethodsList) View() string {
+	return m.list.View()
+}
+
+func (m *MethodsList) SelectedItem() (methodItem, bool) {
+	item, ok := m.list.SelectedItem().(methodItem)
+	return item, ok
+}
+
+type methodItem struct {
+	method protoreflect.MethodDescriptor
+}
+
+func (i methodItem) Title() string       { return string(i.method.FullName()) }
+func (i methodItem) Description() string { return "" }
+func (i methodItem) FilterValue() string { return string(i.method.FullName()) }
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -10,9 +10,20 @@ import (
 
 var _ tea.Model = &Model{}
 
+type viewState int
+
+const (
+	viewServices viewState = iota
+	viewMethods
+)
+
 type Model struct {
+	state        viewState
 	servicesList ServicesList
+	methodsList  *MethodsList
 	grpcClient   *grpc.Client
+	width        int
+	height       int
 }
 
 func NewModel(grpcClient *grpc.Client) (Model, error) {
@@ -22,6 +33,7 @@ func NewModel(grpcClient *grpc.Client) (Model, error) {
 	}
 
 	return Model{
+		state:        viewServices,
 		servicesList: NewServicesList(services),
 		grpcClient:   grpcClient,
 	}, nil
@@ -37,26 +49,63 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
+		case "esc", "backspace":
+			if m.state == viewMethods {
+				m.state = viewServices
+				m.methodsList = nil
+				return m, nil
+			}
 		case "enter":
-			if item, ok := m.servicesList.SelectedItem(); ok {
-				m.servicesList.SetSelected(item.name)
-				// TODO: update tui to show the methods for the selected service
-				methods, err := m.grpcClient.ListMethods(item.name)
+			switch m.state {
+			case viewServices:
+				svc, ok := m.servicesList.SelectedItem()
+				if !ok {
+					fmt.Fprintf(os.Stderr, "no service selected\n")
+					return m, tea.Quit
+				}
+				m.servicesList.SetSelected(svc.name)
+				methods, err := m.grpcClient.ListMethods(svc.name)
 				if err != nil {
 					fmt.Fprintf(os.Stderr, "error listing methods: %v\n", err)
 					return m, tea.Quit
 				}
-				_ = methods // TODO: use methods
+				methodsList := NewMethodsList(svc.name, methods)
+				methodsList.SetSize(m.width, m.height)
+				m.methodsList = &methodsList
+				m.state = viewMethods
+			case viewMethods:
+
 			}
 		}
 	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
 		m.servicesList.SetSize(msg.Width, msg.Height)
+		if m.methodsList != nil {
+			m.methodsList.SetSize(msg.Width, msg.Height)
+		}
 	}
 
-	cmd := m.servicesList.Update(msg)
+	var cmd tea.Cmd
+	switch m.state {
+	case viewMethods:
+		if m.methodsList != nil {
+			cmd = m.methodsList.Update(msg)
+		}
+	default:
+		cmd = m.servicesList.Update(msg)
+	}
 	return m, cmd
 }
 
 func (m Model) View() string {
+	switch m.state {
+	case viewMethods:
+		if m.methodsList != nil {
+			return m.methodsList.View()
+		} else {
+			return "No methods found for selected service"
+		}
+	}
 	return m.servicesList.View()
 }
